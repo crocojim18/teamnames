b77a3d86d0cd5c78ab4ec33bdd0645a269df3bff
Marty Taylor <crocojim18@gmail.com>
2021-04-10 23:11:04 -0400

    Fixes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # teamnames
 Randomly Generated Sports Team Names
+
+## `python generator.py` will make 10 random sports team names
+This all includes:
+* nouns.txt - English plural nouns scraped from Wiktionary
+* places.txt - United States place names scraped from Wiktionary
+* the spider I used to scrape those (change the url in noun.py and run `scrapy crawl noun`)
+** _Scrapy is required to run the spider_
+
+## Have at 'em, folks
--- generator.py
+++ generator.py
@@ -1,5 +1,14 @@
 import random
 
+def titleCase(string):
+	toRet = []
+	for i in string.split(" "):
+		if "a" <= i[0] <= "z":
+			toRet.append(i[0].upper()+i[1:])
+		else:
+			toRet.append(i)
+	return " ".join(toRet)
+
 places = open("places.txt",'r',encoding="utf-8")
 nouns = open("nouns.txt",'r',encoding="utf-8")
 
@@ -10,7 +19,7 @@ for i in range(10):
 	choosePlace = placelist[random.randrange(0,len(placelist))].strip()
 	chooseNoun = nounlist[random.randrange(0,len(nounlist))].strip()
 
-	print("The {} {}".format(choosePlace, chooseNoun).title())
+	print(titleCase("The {} {}".format(choosePlace, chooseNoun)))
 
 places.close()
 nouns.close()
